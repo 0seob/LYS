0bb4f4acc0cc2bb76b8aec0da532912e1b5c4d9b
이영섭 <changas9263@gmail.com>
2022-11-06 15:42:25 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <list>
+#define quot 1000000000
+#define FASTIO() ios::sync_with_stdio(false); cin.tie(NULL);
+#define X first
+#define Y second
+typedef long long ll;
+
+using namespace std;
+
+int board[1005][1005];
+int vis[1005][1005];
+
+int main(int argc, char const *argv[]) {
+  FASTIO();
+  int n, m, cnt = 0, max = 0;
+  queue<pair<int, int> >q;
+  int dx[4] = {1, 0, -1, 0};
+  int dy[4] = {0, 1, 0, -1};
+  cin >> n >> m;
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < m; j++)
+      cin >> board[i][j];
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      if(board[i][j] == 1 && vis[i][j] == 0){
+        q.push(make_pair(i, j));
+        vis[i][j] = 1;
+        cnt++;
+        //cout << i << "," << j << '\n';
+      }
+      int bound = 0;
+      while(!q.empty()){
+        pair<int, int> cur = q.front(); q.pop(); bound++;
+        for(int dir = 0; dir < 4; dir++){
+          int nx = cur.X + dx[dir];
+          int ny = cur.Y + dy[dir];
+          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+          if(vis[nx][ny] || board[nx][ny] != 1) continue;
+          vis[nx][ny] = 1;
+          q.push(make_pair(nx, ny));
+        }
+      }
+      if(bound > max) max = bound;
+    }
+  }
+  cout << cnt << '\n' << max;
+}
