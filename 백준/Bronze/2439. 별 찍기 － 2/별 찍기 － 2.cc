32510d8e000b70c0204708a8ae43e046d823f827
이영섭 <changas9263@gmail.com>
2022-11-13 17:46:07 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int N, i, j, k;
+	scanf("%d", &N);
+	
+	for(i = 1; i <= N; i++){
+		for(j = 0; j < N - i; j++){
+			printf(" ");
+		}
+		for(k = 1; k <= i; k++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
