27e750bb8b9986e9ec0a0b1f4a36236e6c9bd0ca
이영섭 <changas9263@gmail.com>
2022-11-16 17:35:50 +0900

    [Silver I] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <list>
+#define quot 1000000000
+#define FASTIO() ios::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  FASTIO();
+  string s, str, str2;
+  cin >> s;
+  stack<char> st;
+  int temp = 1, ans = 0;
+  for(int i = 0; i < s.length(); i++){
+    if(s[i] == '('){
+      temp *= 2;
+      st.push(s[i]);
+    }
+    else if(s[i] == '['){
+      temp *= 3;
+      st.push(s[i]);
+    }
+    else if(s[i] == ')'){
+      if(st.empty() || st.top() == '['){
+        cout << "0";
+        return 0;
+      }
+      st.pop();
+      if(i > 0 && s[i-1] == '(') ans += temp;
+      temp /= 2;
+    }
+    else if(s[i] == ']'){
+      if(st.empty() || st.top() == '('){
+        cout << "0";
+        return 0;
+      }
+      st.pop();
+      if(i > 0 && s[i-1] == '[') ans += temp;
+      temp /= 3;
+    }
+  }
+  if(!st.empty()){
+    cout << "0";
+    return 0;
+  }
+  cout << ans;
+}
