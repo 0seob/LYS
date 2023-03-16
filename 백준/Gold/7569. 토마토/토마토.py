ab8f9f646fc499205eaf98c4c9ec28d8d6bbfd82
이영섭 <changas9263@gmail.com>
2023-03-17 01:25:19 +0900

    [Gold V] Title: 토마토, Time: 2072 ms, Memory: 61252 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,54 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+def solution():
+    M, N, H = map(int, input().split())
+
+    dq = deque()
+
+    dx = [1, 0, -1, 0, 0, 0]
+    dy = [0, 1, 0, -1, 0, 0]
+    dz = [0, 0, 0, 0, 1, -1]
+
+    arr = [[[0 for _ in range(H)] for _ in range(M)] for _ in range(N)]
+    visit = [[[0 for _ in range(H)] for _ in range(M)] for _ in range(N)]
+
+    for k in range(H):
+        for i in range(N):
+            temp = list(map(int, input().split()))
+            for j in range(M):
+                arr[i][j][k] = temp[j]
+                if temp[j] == 1:
+                    dq.append([i, j, k])
+                    visit[i][j][k] = 1
+                elif temp[j] == -1:
+                    visit[i][j][k] = -1
+    
+    while bool(dq) == True:
+        i, j, k = dq.popleft()
+        for idx in range(len(dx)):
+            nx = dx[idx] + i
+            ny = dy[idx] + j
+            nz = dz[idx] + k
+            if nx < 0 or ny < 0 or nz < 0 or nx >= N or ny >= M or nz >= H:
+                continue
+            if arr[nx][ny][nz] != 0 or visit[nx][ny][nz] != 0 :
+                continue
+            visit[nx][ny][nz] = visit[i][j][k] + 1
+            # arr[nx][ny][nz] = 1
+            dq.append([nx, ny, nz])
+    max_val = 0
+    for i in range(N):
+        for j in range(M):
+            for k in range(H):
+                if visit[i][j][k] == 0:
+                    print(-1)
+                    return
+                if visit[i][j][k] > max_val:
+                    max_val = visit[i][j][k]
+    print(max_val - 1)
+    
+if __name__ == "__main__":
+    solution()
