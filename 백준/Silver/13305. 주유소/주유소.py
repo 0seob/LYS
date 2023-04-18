5a0c868b7f7f20cf86b309417ad5392d6f5d270a
이영섭 <changas9263@gmail.com>
2023-04-18 11:44:13 +0900

    [Silver III] Title: 주유소, Time: 188 ms, Memory: 140596 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,14 +1,9 @@
 N = int(input())
 road = list(map(int, input().split()))
 dpl = list(map(int, input().split()))
-new_dpl = []
-for idx, dp in enumerate(dpl):
-    new_dpl.append((idx, dp))
-new_dpl.sort(key= lambda x: x[1])
+for i in range(1, len(dpl)):
+    dpl[i] = min(dpl[i-1], dpl[i])
 ans = 0
 for idx, r in enumerate(road):
-    for nd in new_dpl:
-        if nd[0] <= idx:
-            ans += nd[1] * r
-            break
+    ans += dpl[idx] * r
 print(ans)
