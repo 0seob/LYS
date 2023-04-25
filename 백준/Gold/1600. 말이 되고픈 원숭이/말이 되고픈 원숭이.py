cad9f57d8b25982783be932bcabf9b3b0e548175
이영섭 <changas9263@gmail.com>
2023-04-25 15:36:12 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 640 ms, Memory: 150512 KB -BaekjoonHub

---
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+K = int(input())
+W, H = map(int, input().split())
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+hx = [-2, -2, -1, -1, 1, 1, 2, 2]
+hy = [-1, 1, -2, 2, -2, 2, -1, 1]
+
+board = [list(map(int, input().split())) for _ in range(H)]
+visit = [[[0]*(K+1) for _ in range(W)] for _ in range(H)]
+
+
+def bfs(horse_move):
+    dq = deque()
+    dq.append((0, 0, 0))
+    visit[0][0][0] = 1
+    while dq:
+        cx, cy, K = dq.popleft()
+        if K < horse_move:
+            for dir in range(8):
+                nx, ny = cx + hx[dir], cy + hy[dir]
+                if nx < 0 or nx >= H or ny < 0 or ny >= W:
+                    continue
+                if board[nx][ny] == 1 or visit[nx][ny][K+1] > 0:
+                    continue
+                dq.append((nx, ny, K+1))
+                visit[nx][ny][K+1] = visit[cx][cy][K] + 1
+        for dir in range(4):
+            nx, ny = cx + dx[dir], cy + dy[dir]
+            if nx < 0 or nx >= H or ny < 0 or ny >= W:
+                continue
+            if board[nx][ny] == 1 or visit[nx][ny][K] > 0:
+                continue
+            dq.append((nx, ny, K))
+            visit[nx][ny][K] = visit[cx][cy][K] + 1
+    ans = 10e8
+    for i in range(horse_move+1):
+        if visit[H-1][W-1][i]:
+            ans = min(ans, visit[H-1][W-1][i])
+    if ans != 10e8:
+        return ans - 1
+    else:
+        return -1
+
+
+print(bfs(K))
