5ef6ce861241ab80d92d2147e7941e773f114925
이영섭 <changas9263@gmail.com>
2023-06-22 17:38:52 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/입국심사.py
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -1,17 +1,16 @@
 def solution(n, times):
     answer = 0
-    left = 1
-    right = max(times) * n
+    left, right = 1, max(times)*n
     while left < right:
         mid = (left + right) // 2
-        people = 0
-        for time in times:
-            people += mid // time
-            if people >= n:
+        check = 0
+        for t in times:
+            check += mid // t
+            if check >= n:
                 break
-        if people >= n:
-            answer = mid
+        if check >= n:
             right = mid
+            answer = mid
         else:
             left = mid + 1
     return answer
